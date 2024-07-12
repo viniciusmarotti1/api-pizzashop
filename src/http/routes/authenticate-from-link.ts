f6eafc538de7dcf5cd51fc1c272e438da1004eba
Vinicius Marotti <vinicius.marottihotmail.com>
2024-07-11 21:25:51 -0300

    feat: create validation and unauthorized errors with Elysia

--- src/http/routes/authenticate-from-link.ts
+++ src/http/routes/authenticate-from-link.ts
@@ -7,8 +7,8 @@ import { eq } from 'drizzle-orm'
 
 export const authenticateFromLink = new Elysia().use(auth).get(
   '/auth-links/authenticate',
-  async ({ query, signUser, set }) => {
-    const { code, redirect } = query
+  async ({ query, signUser /* set */ }) => {
+    const { code /* redirect */ } = query
 
     const authLinkFromCode = await db.query.authLinks.findFirst({
       where(fields, { eq }) {
